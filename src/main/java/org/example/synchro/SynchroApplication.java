824a4df32e0529e2dca3d63976c9796cd2c0ecad
vitor souza <vitordesouzavieira169@gmail.com>
2026-05-03 22:39:51 -0300

    primeiro commit

---
+++ src/main/java/org/example/synchro/SynchroApplication.java
@@ -0,0 +1,13 @@
+package org.example.synchro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynchroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SynchroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/synchro/SynchroApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.synchro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynchroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
